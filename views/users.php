9cd39780ecd53a59b7eeaccc1ea8eec1fb01f37d
anonymousdomain <domainanonymous1@gmail.com>
2021-11-21 18:16:29 +0300

    styling register form

--- views/users.php
+++ views/users.php
@@ -6,26 +6,27 @@
 
 <div class="form">
     <form action="" method="post">
-        <div>
+        <div class="form-group">
             <label for="firstname" >Firstname</label>
             <input type="text" name="firstname" placeholder="firstname">
         </div>
-        <div>
+        <div class="form-group">
             <label for="lastname" >Lastname</label>
             <input type="text" name="lastname" placeholder="lastname">
         </div>
-        <div>
+        <div class="form-group">
             <label for="email" >Email</label>
             <input type="email" name="email" placeholder="email">
         </div>
-        <div>
+        <div class="form-group">
             <label for="password" >Password</label>
             <input type="password" name="password" placeholder="password">
         </div>
-        <div>
+        <div class="form-group">
             <label for="password" >ConfirmPassword</label>
             <input type="password" name="password" placeholder="password">
         </div>
+        <input type="submit" class="btn btn-primary" value="Register">
     </form>
 </div>
 </div>
