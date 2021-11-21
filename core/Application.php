e6526c2853ec41be079f5cff8e2f6f066794f0ab
anonymousdomain <domainanonymous1@gmail.com>
2021-11-21 15:33:07 +0300

    access request class from router through Application using object inversion concept

--- core/Application.php
+++ core/Application.php
@@ -1,12 +1,17 @@
 <?php
 namespace app\core;
+
+use Request;
+
 class Application
 {
 
   public Router $router;
+  Public Request $request;
   public function __construct()
   {
-    $this->router = new Router();
+    $this->request=new Request();
+    $this->router = new Router($this->request);
   }
 
   public function run(){
--- core/Router.php
+++ core/Router.php
@@ -1,24 +1,31 @@
 <?php
 
 namespace app\core;
+
+use Request;
+
 class Router
 {
-
-    protected array $routes=[];
+    public Request $request;
+    protected array $routes = [];
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
     public function get($path, $callback)
     {
 
-       $this->routes['get'][$path]=$callback; 
+        $this->routes['get'][$path] = $callback;
     }
     public function post($path, $callback)
     {
 
-       $this->routes['post'][$path]=$callback; 
+        $this->routes['post'][$path] = $callback;
     }
 
-    public function resolve(){
-
-        
-    
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
     }
 }
