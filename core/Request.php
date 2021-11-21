a99a7dab56b02b718686e584ce86511ef40c402c
anonymousdomain <domainanonymous1@gmail.com>
2021-11-21 15:37:10 +0300

    finding ? and take the path from 0 to ? postision and return the path

--- core/Request.php
+++ core/Request.php
@@ -4,11 +4,18 @@ class Request {
 
     public function getPath(){
 
+        $path=$_SERVER['REQUEST_URI']??'/';
+        $pos=strpos($path,'?');
+
+        if($pos===false){
+            return $path;
+        }
+        return substr($path,0,$pos);
     }
 
     public function getMethod(){
 
 
     }
-    
+
 }
