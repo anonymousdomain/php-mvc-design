ff990d270ef0b1fa422534fabcdd2024062c3bae
anonymousdomain <domainanonymous1@gmail.com>
2021-11-22 11:44:09 +0300

    small changes use name in form

--- views/Register.php
+++ views/Register.php
@@ -7,24 +7,24 @@
   <form>
     <div class="form-group">
       <label for="firstname" class="form-label mt-4">firstname</label>
-      <input type="text" class="form-control" id="firstname" placeholder="Enter firstname">
+      <input type="text" name="firstname" class="form-control" id="firstname" placeholder="Enter firstname">
     </div>
     <div class="form-group">
       <label for="lastname" class="form-label mt-4">lastname</label>
-      <input type="email" class="form-control" id="lastname" placeholder="lastname">
+      <input type="email" name="lastname" class="form-control" id="lastname" placeholder="lastname">
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
       <label for="password" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="password" placeholder="Password">
+      <input type="password" name="password" class="form-control" id="password" placeholder="Password">
     </div>
     <div class="form-group">
       <label for="ConfirmPassword" class="form-label mt-4">ConfirmPassword</label>
-      <input type="password" class="form-control" id="ConfirmPassword" placeholder="ConfirmPassword">
+      <input type="password" name="confirmPassword" class="form-control" id="ConfirmPassword" placeholder="ConfirmPassword">
     </div>
     <button type="submit" class="btn btn-primary btn-lg btn-block mt-4">submit</button>
   </form>
--- views/contact.php
+++ views/contact.php
@@ -6,18 +6,18 @@
 
     <form action="" method="POST">
         <div class="form-group">
-            <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <label class="form-label mt-4">Email address</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1"  placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <label class="form-label mt-4">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
 
         <div class="form-group">
             <label for="exampleTextarea" class="form-label mt-4">Example textarea</label>
-            <textarea class="form-control" id="exampleTextarea" rows="2"></textarea>
+            <textarea class="form-control" name="text-area" id="exampleTextarea" rows="2"></textarea>
         </div>
         <button type="submit" class="mt-4 btn btn-primary">Submit</button>
     </form>
