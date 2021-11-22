dd0d0997cf913e36b1359a5c87450d7235201ede
anonymousdomain <domainanonymous1@gmail.com>
2021-11-22 13:32:11 +0300

    model class to deal with data and database

---
+++ models/RegisterModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\models;
+
+class RegisterModel{
+
+    public string $firstName;
+    public string $lastname;
+    public string $email;
+    public string $passwoed;
+    public string $confirmPassword;
+}
