d5bc107f2fdf82351f2afcf254dca584c53aaaca
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 14:57:11 +0300

    git load dontenv file which hwlps to load config files from .env

--- public/index.php
+++ public/index.php
@@ -5,8 +5,17 @@ use app\controllers\SiteControllers;
 use app\core\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
-$app = new Application(dirname(__DIR__));
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+$app = new Application(dirname(__DIR__), $config);
 
 //$app->router->get('/', function () {return "hello world";});
 
@@ -15,15 +24,15 @@ $app = new Application(dirname(__DIR__));
 //$app->router->get('/contact','contact');
 
 //sitecontrollers
-$app->router->get('/',[SiteControllers::class,'home']);
-$app->router->post('/contact',[SiteControllers::class,'handleContact']);
-$app->router->get('/contact',[SiteControllers::class,'showContact']);
+$app->router->get('/', [SiteControllers::class, 'home']);
+$app->router->post('/contact', [SiteControllers::class, 'handleContact']);
+$app->router->get('/contact', [SiteControllers::class, 'showContact']);
 
 //authControllers
-$app->router->get('/login',[AuthControllers::class,'login']);
-$app->router->post('/login',[AuthControllers::class,'login']);
-$app->router->get('/register',[AuthControllers::class,'register']);
-$app->router->post('/register',[AuthControllers::class,'register']);
+$app->router->get('/login', [AuthControllers::class, 'login']);
+$app->router->post('/login', [AuthControllers::class, 'login']);
+$app->router->get('/register', [AuthControllers::class, 'register']);
+$app->router->post('/register', [AuthControllers::class, 'register']);
 
 
 
