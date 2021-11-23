afe6e1ef120cde18507a13e0de9e4e11df8d257f
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 20:52:55 +0300

    make a change to labels ,it should be user frendliy

--- core/Session.php
+++ core/Session.php
@@ -8,12 +8,11 @@ class Session
     public function __construct()
     {
         session_start();
-        $flashMessages = $_SESSION[self::FLASH_KEY]??[];
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
         foreach ($flashMessages as $key => &$flashMessage) {
             $flashMessage['remove'] = true;
         }
-        $_SESSION[self::FLASH_KEY]=$flashMessages;
-        
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
     }
     public function setFlash($key, $message)
     {
@@ -22,20 +21,20 @@ class Session
             'value' => $message
         ];
     }
-    public function getFlash($key){
+    public function getFlash($key)
+    {
 
-        return $_SESSION[self::FLASH_KEY][$key]['value']??false;
+        return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
     }
 
     public function __destruct()
     {
-        $flashMessages = $_SESSION[self::FLASH_KEY]??[];
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
         foreach ($flashMessages as $key => &$flashMessage) {
-            if($flashMessage['remove']){
+            if ($flashMessage['remove']) {
                 unset($flashMessages[$key]);
             }
         }
-        $_SESSION[self::FLASH_KEY]=$flashMessages;
-        
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
     }
 }
