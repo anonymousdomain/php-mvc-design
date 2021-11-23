12ed4848a2237720a778ff9befa05b87e4740ea2
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 18:37:52 +0300

    hash the password

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -20,6 +20,7 @@ class RegisterModel extends DbModel
     }
     public function register()
     {
+        $this->password=password_hash($this->password,PASSWORD_DEFAULT);
        return $this->save();
     }
 
