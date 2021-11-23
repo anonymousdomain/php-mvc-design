f6bddf672de6c07860c77ee533cd8093bd57080d
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 15:28:33 +0300

    create migrations table

--- core/Database.php
+++ core/Database.php
@@ -1,20 +1,31 @@
-<?php 
+<?php
 
 namespace app\core;
 
 use PDO;
 
-class Database{
+class Database
+{
 
     public PDO $pdo;
 
     public function __construct(array $config)
     {
-        $dsn=$config['dsn']??'';
-        $user=$config['user']??'';
-        $password=$config['password']??'';
-        $this->pdo=new PDO($dsn,$user,$password);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+        $this->pdo = new PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    public function applyMigrations()
+    {
+
+    }
+    public function createMigrationsTable(){
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations(
+            id INT AUTO_INCREMENT primary key,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFUALT CURRENT_TIMESTAMP
+        )ENGINE=INNODB;");
     }
 }
