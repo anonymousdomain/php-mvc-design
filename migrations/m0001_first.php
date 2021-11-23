74182bd9125d028e37ed547f2efed480f7e84530
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 14:05:01 +0300

    migratios script file will be here

---
+++ migrations/m0001_first.php
@@ -0,0 +1,16 @@
+<?php
+
+ namespace app\migrations;
+
+ class m0001_first{
+
+
+    public function up(){
+
+        echo "migrate";
+    }
+    public function down(){
+
+        echo "down migrate";
+    }
+ }
