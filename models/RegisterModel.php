bbd77ac063428e1881d2893f2af4a6823496df8a
anonymousdomain <domainanonymous1@gmail.com>
2021-11-23 18:47:33 +0300

    set the status code manually

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -7,39 +7,42 @@ use app\core\Model;
 
 class RegisterModel extends DbModel
 {
+     const ACTIVE = 1;
+     const IN_ACTIVE = 0;
+     const DELETED = 2;
 
-    public string $firstname='';
-    public string $lastname='';
-    public string $email='';
-    public string $password='';
-    public string $confirmPassword='';
-
+    public string $firstname = '';
+    public string $lastname = '';
+    public string $email = '';
+    public string $password = '';
+    public string $confirmPassword = '';
+    public int $status=self::ACTIVE;
     public function tableName(): string
     {
         return 'users';
     }
     public function register()
     {
-        $this->password=password_hash($this->password,PASSWORD_DEFAULT);
-       return $this->save();
+        $this->status;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return $this->save();
     }
 
- public function rules(): array
- {
-    return [
-        'firstname' => [self::RULE_REQUIRED],
-        'lastname' => [self::RULE_REQUIRED],
-        'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
-            self::RULE_UNIQUE, 'class' => self::class
-        ]],
-        'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8],[self::RULE_MAX,'max'=>12]],
-        'confirmPassword' => [self::RULE_REQUIRED,[self::RULE_MATCH, 'match' => 'password']],
-    ];
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
+                self::RULE_UNIQUE, 'class' => self::class
+            ]],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 12]],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
+    }
 
- }
-
- public function attribute(): array
- {
-     return['firstname','lastname','email','password'];
- }
+    public function attribute(): array
+    {
+        return ['firstname', 'lastname', 'email','status', 'password'];
+    }
 }
