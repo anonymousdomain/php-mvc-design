33f34fe0894f967491975c0c3c8d59d5ba5714c8
anonymousdomain <domainanonymous1@gmail.com>
2021-11-24 10:31:06 +0300

    push /pass midlwares

--- core/Controller.php
+++ core/Controller.php
@@ -2,18 +2,26 @@
 
 namespace app\core;
 
+use app\core\middlewares\BaseMiddleware;
 
 class Controller
 {
+    /**@var app\core\middlewares\BaseMiddleware[] */
 
+    public array $middlewares = [];
     public string $layout = 'main';
     public function setLayout($layout)
     {
-        $this->layout=$layout;
+        $this->layout = $layout;
     }
     public function render($view, $params = [])
     {
 
         return Application::$app->router->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
     }
 }
