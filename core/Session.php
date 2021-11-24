98b3d9f2dbefc39c35e363b2b3d9d6ec78bfb1c0
anonymousdomain <domainanonymous1@gmail.com>
2021-11-24 08:45:53 +0300

    impliment set() get() and remove() session

--- core/Session.php
+++ core/Session.php
@@ -26,6 +26,20 @@ class Session
 
         return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
     }
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return $_SESSION[$key] ?? false;
+    }
+
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
 
     public function __destruct()
     {
